09c534bada783a70ddc9a7f852a3a9901fbfad5a
Raymond Yang <thelife1988@gmail.com>
2018-11-16 02:24:00 +0800

    Add .gitignore

---
+++ bloglife/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BloglifeConfig(AppConfig):
+    name = 'bloglife'
--- life/settings.py
+++ life/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bloglife',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
